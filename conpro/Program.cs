412599b35a111c79ea1ad4f03fdf05171cbd9014
zeke2400 <skyreg2400@gmail.com>
2014-12-04 10:46:12 +0100

    sql

---
+++ conpro/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conpro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataTable data = sqlcn.GetSampleData();
+            foreach (DataRow row in data.Rows)
+            { 
+            Console.WriteLine(row[0].ToString());
+            Console.WriteLine();
+        }
+            Console.ReadKey();
+
+          
+
+        }
+        public class medarbejder
+        {
+            public string fornavn;
+            public string efternavn;
+            public int id;
+            public List<int> tlf;
+        }
+
+
+
+        public class order
+        {
+            public string fornavn;
+            public string efternavn;
+            public int id;
+            public List<int> tlf;
+        }
+
+
+        static List<medarbejder> liste = new List<medarbejder> 
+        {
+            new medarbejder {fornavn = "mark",efternavn = " smith",id=0,tlf = new List<int>{123456,135790}},
+             new medarbejder {fornavn = "hans",efternavn = " larsen",id=0,tlf = new List<int>{852852,852369}},
+              new medarbejder {fornavn = "jens",efternavn = " jensen",id=0,tlf = new List<int>{741147,789123}},
+               new medarbejder {fornavn = "peter",efternavn = " petersen",id=0,tlf = new List<int>{951159,755357}},
+                new medarbejder {fornavn = "kim",efternavn = " kimsen",id=0,tlf = new List<int>{751595,753595}},
+                 new medarbejder {fornavn = "lasse",efternavn = " las",id=0,tlf = new List<int>{149675,358575}}
+        };
+
+        static List<order> order_list = new List<order> 
+        {
+            new order {fornavn = "mark",efternavn = " smith",id=0,tlf = new List<int>{123456,135790}},
+             new order {fornavn = "hans",efternavn = " larsen",id=0,tlf = new List<int>{852852,852369}},
+              new order {fornavn = "jens",efternavn = " jensen",id=0,tlf = new List<int>{741147,789123}},
+               new order {fornavn = "peter",efternavn = " petersen",id=0,tlf = new List<int>{951159,755357}},
+                new order {fornavn = "kim",efternavn = " kimsen",id=0,tlf = new List<int>{751595,753595}},
+                 new order {fornavn = "lasse",efternavn = " las",id=0,tlf = new List<int>{149675,358575}}
+        };
+
+
+
+
+       
+
+
+
+    }
+}
---
+++ conpro/sqlcn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conpro
+{
+     static class sqlcn
+    {
+         
+          // static string connectionString = "Data Source=(localdb)\\ProjectsV12;Initial Catalog=Database1;Integrated Security=True";
+           // SqlConnection conn = new SqlConnection(connectionString);
+
+
+         public static DataTable GetSampleData()
+         {
+             SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\ProjectsV12;Initial Catalog=Database1;Integrated Security=True");
+             SqlCommand cmd = new SqlCommand("Select * from users", connection);
+             SqlDataAdapter da = new SqlDataAdapter(cmd);
+             DataSet ds = new DataSet();
+
+             try
+             {
+                 connection.Open();
+                 da.Fill(ds);
+             }
+          
+             finally
+             {
+                 if (connection.State == System.Data.ConnectionState.Open)
+                     connection.Close();
+             }
+
+             return ds.Tables[0];
+         }
+         
+    }
+}
